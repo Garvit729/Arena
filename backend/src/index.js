d4e7224196afa8724206b4a099006bc1832897f1
Garv729 <garvitjain729@gmail.com>
2025-07-25 12:30:05 +0530

    c2

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://arena-1-ogtu.onrender.com',
     credentials: true 
 }))
 
